aab5884a7bec52cd080b9862e914346a0d9af657
azoong <shshinkitec2@gmail.com>
2022-07-11 19:55:07 +0900

    페이지 연결

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+@app.route('/post')
+def post():
+    return render_template("post.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/review')
+def join():
+    return render_template("review.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
