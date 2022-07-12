e3741c0d747f4cf12b3566034ac7b23970711460
JiyuJang <apryll7995@gmail.com>
2022-07-12 20:02:02 +0900

    test

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from requests import request
 
 load_dotenv()
 mongodbUri = os.environ.get('mongodbUri')
@@ -33,6 +34,25 @@ def login():
 @app.route('/join')
 def join():
     return render_template("join.html")
+
+
+@app.route('/signUp', methods=["GET"])
+def idCheck_dup():
+    id_list = list(db.users.find({}, {"_id": False, "pw": False}))
+    return jsonify(id_list)
+
+
+@app.route('/signUp', methods=["POST"])
+def signUp():
+    id_receive = request.form['id']
+    pw_receive = request.form['pw']
+
+    doc = {
+        "userID": id_receive,
+        "pw": pw_receive,
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 
 @app.route('/review')
